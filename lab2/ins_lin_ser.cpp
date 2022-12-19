61d555d0fa4c17241234450c958b61f118ec6192
Gerina Mary C <108613543+Gerina-MC@users.noreply.github.com>
2022-12-19 19:45:01 +0530

    Update ins_lin_ser.cpp

--- lab2/ins_lin_ser.cpp
+++ lab2/ins_lin_ser.cpp
@@ -23,7 +23,7 @@ int main()
         a[j+1]=k;
     }
     int k;
-    cout<<"Enter the element to search"<<endl;
+    cout<<"Enter the element to search:"<<endl;
     cin>>k;
     int f=0;
     for(int i=0;i<n;i++)
