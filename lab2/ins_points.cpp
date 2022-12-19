6a83474577ef819f95ae084888ec0e51556e9a91
Gerina Mary C <108613543+Gerina-MC@users.noreply.github.com>
2022-12-19 19:44:03 +0530

    Update and rename abc.cpp to ins_points.cpp

--- lab2/ins_points.cpp
+++ lab2/ins_points.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
-#include <math>
+#include <math.h>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number of elements:";
+    cout<<"Enter the number of points:";
     cin>>n;
     int a[n][2];
-    int b[n];
-    cout<<"Enter the elements:"<<endl;
+    float b[n];
+    cout<<"Enter the x and y values:"<<endl;
     for(int i=0;i<n;i++)
     {
-        int s=0;
+        float s=0;
         for(int j=0;j<2;j++)
         {
             cin>>a[i][j];
             s+=a[i][j]*a[i][j];
         }
-        b[i]=math.sqrt(s);
+        b[i]=sqrt(s);
     }
     for(int i=1;i<n;i++)
     {
-        int k=b[i];
+        float k=b[i];
         int k1=a[i][0];
         int k2=a[i][1];
         int j=i-1;
         while(j>-1 && b[j]>k)
         {
-            b[j+1]=a[j];
+            b[j+1]=b[j];
             a[j+1][0]=a[j][0];
             a[j+1][1]=a[j][1];
             j--;
@@ -36,13 +36,9 @@ int main()
         a[j+1][0]=k1;
         a[j+1][1]=k2;
     }
-    cout<<"Sorted elements:"<<endl;
+    cout<<"Sorted points:"<<endl;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<2;j++)
-        {
-            cout<<a[j][0]<<" "<<a[j][1];
-        }
-        cout<<endl;
+        cout<<a[i][0]<<" "<<a[i][1]<<endl;
     }
 }
